8d31a30c14693223ca358b5c7849619de62b8401
sakura <1969314158@qq.com>
2023-09-05 17:28:48 +0800

    增加账号密码

--- src/main/java/com/lhs/entity/survey/SurveyUser.java
+++ src/main/java/com/lhs/entity/survey/SurveyUser.java
@@ -13,6 +13,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class SurveyUser {
     private Long id;   //唯一id （自增
+    private String passWord;
     private Long uid;
     private String userName;  //用户id(用户填写的昵称后加#xxxx
     private Date createTime;  //创建时间
