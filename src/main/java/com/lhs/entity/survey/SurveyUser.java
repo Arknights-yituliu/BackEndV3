e96f8aa011586883dc0a4f416d2c75014a259948
sakura <1969314158@qq.com>
2023-09-05 17:27:43 +0800

    增加账号名称限制

--- src/main/java/com/lhs/entity/survey/SurveyUser.java
+++ src/main/java/com/lhs/entity/survey/SurveyUser.java
@@ -13,7 +13,6 @@ import java.util.Date;
 @NoArgsConstructor
 public class SurveyUser {
     private Long id;   //唯一id （自增
-    private String passWord;
     private Long uid;
     private String userName;  //用户id(用户填写的昵称后加#xxxx
     private Date createTime;  //创建时间
