46dbd318d32ffe0a3d65b4fc8b1455054a26b0c2
sakura <1969314158@qq.com>
2023-09-21 17:52:22 +0800

    补充mapper文件

---
+++ src/main/java/com/lhs/mapper/DeveloperMapper.java
@@ -0,0 +1,10 @@
+package com.lhs.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lhs.entity.other.Developer;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeveloperMapper extends BaseMapper<Developer> {
+
+}
