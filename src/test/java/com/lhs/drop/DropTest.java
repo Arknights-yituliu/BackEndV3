093748f874f50e783d3a2cdf18b2877562a98e02
yamazakura <1969314158@qq.com>
2026-05-29 01:15:52 +0800

    chore:导出关卡掉率

--- src/test/java/com/lhs/drop/DropTest.java
+++ src/test/java/com/lhs/drop/DropTest.java
@@ -1,35 +1,49 @@
 package com.lhs.drop;
 
+import com.lhs.common.util.FileUtil;
+import com.lhs.common.util.JsonMapper;
+import com.lhs.common.util.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.lhs.common.enums.TimeGranularity;
+import com.lhs.entity.dto.drop.StageDropCount;
+import com.lhs.entity.po.material.StageDropStatistics;
+import com.lhs.mapper.material.StageDropStatisticsMapper;
 import com.lhs.service.material.StageDropStatisticsService;
 
 import jakarta.annotation.Resource;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
-public class DropTest  {
+public class DropTest {
 
     @Resource
     private StageDropStatisticsService stageDropStatisticsService;
 
+    @Resource
+    private StageDropStatisticsMapper stageDropStatisticsMapper;
+
     @Test
     public void testStageDropStatistics() throws Exception {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH");
-        Date start = sdf.parse("2026-02-21 00");
+        Date start = sdf.parse("2026-05-01 00");
         long oneHour = 1000 * 60 * 60;
         Date end = new Date(start.getTime() + oneHour);
 
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000; i++) {
             Calendar cal = Calendar.getInstance();
             cal.setTime(start);
             int month = cal.get(Calendar.MONTH) + 1;
             int year = cal.get(Calendar.YEAR);
-           String tableName = String.format("stage_drop_%d_%02d", year, month);
+            String tableName = String.format("stage_drop_%d_%02d", year, month);
             // String tableName = "stage_drop_20251130_20260221";
             stageDropStatisticsService.stageDropHourlyStatistics(start, end, tableName);
             start = new Date(start.getTime() + oneHour);
@@ -38,45 +52,50 @@ public class DropTest  {
     }
 
     @Test
-    public void testStageDropStatistics2() throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH");
-        Date start = sdf.parse("2026-02-22 00");
-        long oneHour = 1000 * 60 * 60;
-        Date end = new Date(start.getTime() + oneHour);
+    public void testSelect() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date start = sdf.parse("2025-01-01");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(start);
+        Date now = new Date();
 
-        for (int i = 0; i < 10000; i++) {
-            Calendar cal = Calendar.getInstance();
+        Map<String, StageDropCount> collect = new HashMap<>();
+        List<StageDropStatistics> list = new ArrayList<>();
+        while (start.before(now)) {
             cal.setTime(start);
-            int month = cal.get(Calendar.MONTH) + 1;
-            int year = cal.get(Calendar.YEAR);
-//            String tableName = "stage_drop_" + year + "_" + month;
-            String tableName = "stage_drop_20260222_20260328";
-            stageDropStatisticsService.stageDropHourlyStatistics(start, end, tableName);
-            start = new Date(start.getTime() + oneHour);
-            end = new Date(end.getTime() + oneHour);
-        }
-    }
+            cal.add(Calendar.MONTH, 1);
+            Date end = cal.getTime();
 
-    @Test
-    public void testStageDropStatistics3() throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH");
-        Date start = sdf.parse("2026-02-22 00");
-        long oneHour = 1000 * 60 * 60;
-        Date end = new Date(start.getTime() + oneHour);
-
-        for (int i = 0; i < 10000; i++) {
-            if(start.compareTo(sdf.parse("2026-04-01 19")) > 0){
-                break;
+            list = stageDropStatisticsMapper.listByDate(
+                    TimeGranularity.HOUR.code(), start, end);
+            System.out.println(sdf.format(start) + " ~ " + sdf.format(end) + " 数据量: " + list.size());
+            for (StageDropStatistics item : list) {
+                String key = item.getStageId() + "_" + item.getItemId();
+                StageDropCount count = collect.getOrDefault(key, new StageDropCount(item.getStageId(),item.getItemId(),0,0));
+                count.addQuantity(item.getQuantity());
+                count.addTimes(item.getTimes());
+                collect.put(key, count);
             }
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(start);
-            int month = cal.get(Calendar.MONTH) + 1;
-            int year = cal.get(Calendar.YEAR);
-//            String tableName = "stage_drop_" + year + "_" + month;
-            String tableName = "stage_drop_20260222_20260328";
-            stageDropStatisticsService.stageDropHourlyStatistics(start, end, tableName);
-            start = new Date(start.getTime() + oneHour);
-            end = new Date(end.getTime() + oneHour);
+            start = end;
         }
+        
+        List<StageDropStatistics> result = new ArrayList<>();
+        for(Map.Entry<String,StageDropCount> entry: collect.entrySet()){
+            String key = entry.getKey();
+            StageDropCount count = entry.getValue();
+
+            StageDropStatistics item = new StageDropStatistics();
+            item.setStageId(count.getStageId());
+            item.setItemId(count.getItemId());
+            item.setQuantity(count.getQuantity());
+            item.setTimes(count.getTimes());
+            result.add(item);
+        }
+
+        FileUtil.saveJsonFile("C:\\Users\\admin\\Desktop\\import\\", "stage_drop.json", JsonMapper.toJSONString(result));
+        
+        StageDropCount stageDropCount = collect.get("main_01-07_30012");
+        Logger.info(String.valueOf(stageDropCount.getQuantity()*1.0/stageDropCount.getTimes()));
+
     }
 }
--- src/test/java/com/lhs/drop/StageDropTest.java
+++ src/test/java/com/lhs/drop/StageDropTest.java
@@ -24,41 +24,19 @@ public class StageDropTest {
     @Resource
     private StageDropMapper stageDropMapper;
 
-    @Test
-    public void getActivityDrop() throws Exception {
-        List<StageDrop> tmpList = new ArrayList<>();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
-        Date start = sdf.parse("2025-06-19 00:00:00");
-        long oneDay = 1000 * 60 * 60 * 24;
-        Date end = new Date(start.getTime() + oneDay);
-
-        for (int i = 0; i < 15; i++) {
-
-            Logger.info("查询日期：" + sdf.format(start) + "至" + sdf.format(end));
-            List<StageDrop> list = stageDropMapper.listOldStageDropByDate("stage_drop_20250527_20250808", start, end);
-
-            tmpList = list.stream().filter(e -> "act43side_06".equals(e.getStageId())).toList();
-            Logger.info("查询结果数量：" + tmpList.size());
-            FileUtil.saveJsonFile("D:\\stageDrop\\AD-6\\", sdf2.format(start) + ".json",
-                    JsonMapper.toJSONString(tmpList));
-            start.setTime(start.getTime() + oneDay);
-            end.setTime(end.getTime() + oneDay);
-        }
-
-    }
+  
 
     @Test
     void importStageDrop() throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date start = sdf.parse("2024-12-01 00:00:00");
+        Date start = sdf.parse("2026-05-01 00:00:00");
         long oneDay = 1000 * 60 * 60 * 24;
         Date end = new Date(start.getTime() + oneDay);
         for (int i = 0; i < 100; i++) {
             Calendar cal = Calendar.getInstance();
             cal.setTime(start);
-            if (cal.get(Calendar.MONTH) == Calendar.JANUARY) {
-                 Logger.info("start已进入一月，退出循环");
+            if (cal.get(Calendar.MONTH) == Calendar.JUNE) {
+                 Logger.info("start已进入六月，退出循环");
                 break;
             }
 //            Date deadline = sdf.parse("2025-02-10 00:00:00");
@@ -67,7 +45,7 @@ public class StageDropTest {
 //                break;
 //            }
 
-            List<StageDrop> list = stageDropMapper.listOldStageDropByDate("stage_drop", start, end);
+            List<StageDrop> list = stageDropMapper.listOldStageDropByDate("stage_drop_20260329_20260512", start, end);
 
             Logger.info("查询日期：" + sdf.format(start) + "至" + sdf.format(end) + "，数量：" + list.size());
 
@@ -82,7 +60,7 @@ public class StageDropTest {
             for (int j = 0; j < totalSize; j += batchSize) {
                 int endIdx = Math.min(j + batchSize, totalSize);
                 List<StageDrop> chunk = list.subList(j, endIdx);
-                stageDropMapper.insertBatchByTable("stage_drop_2024_12", chunk);
+                stageDropMapper.insertBatchByTable("stage_drop_2026_05", chunk);
                 Logger.info("已插入 " + chunk.size() + " 条，进度 " + endIdx + "/" + totalSize);
             }
         }
