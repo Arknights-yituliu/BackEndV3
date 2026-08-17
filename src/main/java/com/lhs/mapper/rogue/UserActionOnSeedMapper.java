edf2eb667857871f3818786f29fea4e13a05a004
yamasakura <1969314158@qq.com>
2026-08-17 20:16:02 +0800

    chore:清理部分没用的代码

--- src/main/java/com/lhs/mapper/rogue/UserActionOnSeedMapper.java
+++
@@ -1,7 +0,0 @@
-package com.lhs.mapper.rogue;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lhs.entity.po.rogue.UserActionOnSeed;
-
-public interface UserActionOnSeedMapper extends BaseMapper<UserActionOnSeed> {
-}
