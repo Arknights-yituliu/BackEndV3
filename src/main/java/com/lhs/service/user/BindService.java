62903641bf254f8e281a6cb9fe178db647edac03
yamazakura <1969314158@qq.com>
2026-08-25 01:54:03 +0800

    fix:去除一些无用的api和无用字段

--- src/main/java/com/lhs/service/user/BindService.java
+++ src/main/java/com/lhs/service/user/BindService.java
@@ -1,45 +1,9 @@
 package com.lhs.service.user;
 
 import com.lhs.entity.dto.user.AkPlayerBindInfoDTO;
-import com.lhs.entity.dto.user.EmailRequestDTO;
-import com.lhs.entity.dto.user.UpdateUserDataDTO;
 import com.lhs.entity.vo.survey.UserInfoVO;
-import jakarta.servlet.http.HttpServletRequest;
 
 public interface BindService {
-
-    /**
-     * 发送邮件验证码
-     *
-     * @param httpServletRequest HTTP请求对象
-     * @param emailRequestDto    邮件请求数据
-     */
-    void sendVerificationCode(HttpServletRequest httpServletRequest, EmailRequestDTO emailRequestDto);
-
-    /**
-     * 发送更新邮箱的验证码
-     *
-     * @param httpServletRequest HTTP请求对象
-     * @param emailRequestDto    邮箱请求信息
-     */
-    void sendUpdateEmailVerificationCode(HttpServletRequest httpServletRequest, EmailRequestDTO emailRequestDto);
-
-    /**
-     * 检查验证码
-     *
-     * @param httpServletRequest HTTP请求对象
-     * @param verificationCode   验证码
-     * @return 成功消息
-     */
-    String checkVerificationCode(HttpServletRequest httpServletRequest, String verificationCode);
-
-    /**
-     * 绑定邮箱
-     *
-     * @param httpServletRequest HTTP请求对象
-     * @param updateUserDataDto  用户信息
-     */
-    void bindEmail(HttpServletRequest httpServletRequest, UpdateUserDataDTO updateUserDataDto);
 
     /**
      * 备份用户外部账号绑定数据到腾讯云COS
